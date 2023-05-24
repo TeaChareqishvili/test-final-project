dedf6c81bdd957d4288a3715455efaf1dc5f2d5d
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-24 19:52:17 +0400

    answer's background color

--- src/Hooks/UseFetchQuestions.js
+++ src/Hooks/UseFetchQuestions.js
@@ -1,28 +1,20 @@
-import { useState, useEffect } from "react"
-
-
-
+import { useState, useEffect } from "react";
 
 const response =
-"https://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db";
+  "https://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db";
 
-
-const UseFetchQuestions=()=>{
-
-
+const UseFetchQuestions = () => {
   const [list, setList] = useState();
 
+  useEffect(() => {
+    fetch(response)
+      .then((response) => response.json())
+      .then((json) => setList(json));
+  }, []);
 
-    useEffect(() => {
-        fetch(response)
-          .then((response) => response.json())
-          .then((json) => setList(json));
-      }, []);
+  return {
+    list,
+  };
+};
 
-      return {
-        list
-      };
-    
-}
-
-export {UseFetchQuestions}
+export { UseFetchQuestions };
