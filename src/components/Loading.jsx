43a6e442de0ea63b24ada16db576189d4b52467e
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-27 15:54:55 +0400

    change API

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,10 @@
-import { ColorRing } from "react-loader-spinner";
+import { MagnifyingGlass } from "react-loader-spinner";
 import "./HomePageStyle.scss";
 
 function Loading() {
   return (
     <div>
-      <ColorRing 
-       visible={true}
-       height="80"
-       width="80"
-       ariaLabel="blocks-loading"
-       wrapperStyle={{}}
-       wrapperClass="blocks-wrapper"
-       colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']} />
+      <MagnifyingGlass />
     </div>
   );
 }
