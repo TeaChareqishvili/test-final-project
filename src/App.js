637f123bd43e9abed646086e42bb3230b3032e7f
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-27 18:34:33 +0400

    update whole quiz

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Home } from "./components/Home";
 import { SecondPage } from "./components/SecondPage";
 import { QuizTest } from "./components/QuizTest";
 import { ThirdPage } from "./components/ThirdPage";
+import { FourthPage } from "./components/FourthPage";
+import { LastPage } from "./components/LastPage";
 function App() {
   return (
     <div className="App">
@@ -12,6 +14,8 @@ function App() {
         <Route path="/quiztest" element={<QuizTest />} />
         <Route path="/secondpage" element={<SecondPage/>} />
         <Route path="/thirdpage" element={<ThirdPage/>} />
+        <Route path="/fourthpage" element={<FourthPage/>} />
+        <Route path="/lastpage" element={<LastPage/>} />
       </Routes>
     </div>
   );
